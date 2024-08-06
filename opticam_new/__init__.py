7b1e05f88699f35ae9f18a2558ab6ac175deed51
zairving <zackery.irving@icloud.com>
2024-08-06 15:59:09 +0100

    chore: updated init to import some more commonly used routines

--- opticam_new/__init__.py
+++ opticam_new/__init__.py
@@ -1,8 +1,9 @@
 
 from opticam_new.background import Background
-from opticam_new.finder import Finder
+from opticam_new.finder import CrowdedFinder, Finder
 from opticam_new.reducer import Reducer
 from opticam_new.photometer import Photometer
+from opticam_new.local_background import LocalBackground, CircularLocalBackground, EllipticalLocalBackground
 
 
 
