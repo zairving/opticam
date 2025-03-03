e57406b13dead788922461f87fc7606e8ba6e4d4
zairving <zackery.irving@icloud.com>
2025-03-03 16:39:01 +0000

    misc: included unit tests for local background estimators

--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,10 @@
 import unittest
 import numpy as np
 from numpy.typing import NDArray
+from photutils.segmentation import SourceCatalog
 
 from opticam_new.background import Background
+from opticam_new.local_background import CircularLocalBackground, EllipticalLocalBackground
 from opticam_new.finder import CrowdedFinder, Finder
 
 
@@ -49,8 +51,8 @@ class TestBackground(unittest.TestCase):
         bkg_estimator = Background()
         bkg = bkg_estimator(image)
         
-        self.assertTrue(np.allclose(bkg.background, 100, rtol=.01))  # check background is within 1% of true value
-        self.assertTrue(np.allclose(bkg.background_rms, 10, rtol=.05))  # check background RMS is within 5% of true value
+        self.assertTrue(np.allclose(bkg.background, 100, rtol = .01))  # check background is within 1% of true value
+        self.assertTrue(np.allclose(bkg.background_rms, 10, rtol = .05))  # check background RMS is within 5% of true value
 
 
 class TestFinder(unittest.TestCase):
@@ -87,3 +89,54 @@ class TestCrowdedFinder(unittest.TestCase):
         segment_map = finder(image - bkg.background, 5 * bkg.background_rms)
         
         self.assertTrue(segment_map.nlabels == 3)  # check that all three sources were found
+
+
+class TestCircularLocalBackground(unittest.TestCase):
+    
+    def test_circular_local_background(self):
+        """
+        Test the local background estimator.
+        """
+        
+        image = generate_image()
+        
+        bkg_estimator = Background()
+        bkg = bkg_estimator(image)
+        
+        finder = Finder()
+        segment_map = finder(image - bkg.background, 5 * bkg.background_rms)
+        tbl = SourceCatalog(image - bkg.background, segment_map, background=bkg.background).to_table()
+        coords = np.array([tbl["xcentroid"], tbl["ycentroid"]]).T
+        
+        local_bkg_estimator = CircularLocalBackground()
+        
+        for i in range(len(coords)):
+            local_bkg, local_bkg_error = local_bkg_estimator(image, np.sqrt(image), 50, 50, 0, coords[i])
+            self.assertTrue(np.allclose(local_bkg, 100, rtol = 0.01))  # check local background is within 1% of true value
+            self.assertTrue(np.allclose(local_bkg_error, 10, rtol = 0.05))  # check local background error is within 5% of true value
+
+
+
+class TestEllipticalLocalBackground(unittest.TestCase):
+    
+    def test_elliptical_local_background(self):
+        """
+        Test the local background estimator.
+        """
+        
+        image = generate_image()
+        
+        bkg_estimator = Background()
+        bkg = bkg_estimator(image)
+        
+        finder = Finder()
+        segment_map = finder(image - bkg.background, 5 * bkg.background_rms)
+        tbl = SourceCatalog(image - bkg.background, segment_map, background=bkg.background).to_table()
+        coords = np.array([tbl["xcentroid"], tbl["ycentroid"]]).T
+        
+        local_bkg_estimator = EllipticalLocalBackground()
+        
+        for i in range(len(coords)):
+            local_bkg, local_bkg_error = local_bkg_estimator(image, np.sqrt(image), 50, 50, 0, coords[i])
+            self.assertTrue(np.allclose(local_bkg, 100, rtol = 0.01))
+            self.assertTrue(np.allclose(local_bkg_error, 10, rtol = 0.05))
