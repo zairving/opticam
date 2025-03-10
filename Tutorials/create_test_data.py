7b4c5d44ff36901740d25129ddedb2c50e48eef9
zairving <zackery.irving@icloud.com>
2025-03-10 12:08:12 +0000

    misc: test data now contain circular aperture shadow

--- Tutorials/create_test_data.py
+++ Tutorials/create_test_data.py
@@ -3,10 +3,38 @@ import os
 from tqdm import tqdm
 import numpy as np
 from numpy.typing import NDArray
+from typing import List
+
+from create_flats import apply_flat_field
 
 
 def add_two_dimensional_gaussian_to_image(image: NDArray, x_centroid: float, y_centroid: float, peak_flux: float,
                                           sigma_x: float, sigma_y: float, theta: float) -> NDArray:
+    """
+    Add a source to an image.
+    
+    Parameters
+    ----------
+    image : NDArray
+        The image.
+    x_centroid : float
+        The x-coordinate of the source.
+    y_centroid : float
+        The y-coordinate of the source.
+    peak_flux : float
+        The peak flux of the source.
+    sigma_x : float
+        The standard deviation of the source in the x-direction.
+    sigma_y : float
+        The standard deviation of the source in the y-direction.
+    theta : float
+        The rotation angle of the source.
+    
+    Returns
+    -------
+    NDArray
+        The image with the source added.
+    """
     
     x, y = np.meshgrid(np.arange(image.shape[1]), np.arange(image.shape[0]))
     
@@ -20,39 +48,76 @@ def add_two_dimensional_gaussian_to_image(image: NDArray, x_centroid: float, y_c
 
 
 def variable_function(i: float) -> float:
+    """
+    Create a variable flux.
+    
+    Parameters
+    ----------
+    i : float
+        The time.
+    
+    Returns
+    -------
+    float
+        The flux.
+    """
     
     return 5 * np.sin(2 * np.pi * i / 5.5)
 
 
-def create_image(filters, N_sources, variable_source, source_positions, peak_fluxes, i, binning_scale, overwrite):
-    
-    if os.path.isfile(f"Data/{filters[0]}-band_image_{i}.fits") and not overwrite:
-        return
+def create_base_image(i: int, binning_scale: int) -> NDArray:
     
     rng = np.random.default_rng(i)
     
     base_image = np.zeros((int(2048 / binning_scale), int(2048 / binning_scale))) + 100  # create blank image
     noisy_image = base_image + np.sqrt(base_image) * rng.standard_normal(base_image.shape)  # add Poisson noise
     
-    # add background gradient
-    ny, nx = noisy_image.shape
-    y, x = np.mgrid[:ny, :nx]
-    noisy_image = noisy_image
+    return noisy_image
+
+
+def create_images(filters: List[str], N_sources: int, variable_source: int, source_positions: NDArray,
+                 peak_fluxes: NDArray, i: int, binning_scale: int, overwrite: bool) -> None:
+    """
+    Create an image for each filter.
+    
+    Parameters
+    ----------
+    filters : List[str]
+        The filters.
+    N_sources : int
+        The number of sources.
+    variable_source : int
+        The index of the variable source.
+    source_positions : NDArray
+        The positions of the sources.
+    peak_fluxes : NDArray
+        The peak fluxes of the sources.
+    i : int
+        The time.
+    binning_scale : int
+        The binning scale.
+    overwrite : bool
+        Whether to overwrite the image if it already exists.
+    """
     
     for fltr in filters:
         
-        filter_image = noisy_image
+        if os.path.isfile(f"Data/{fltr}-band_image_{i}.fits") and not overwrite:
+            continue
+        
+        noisy_image = create_base_image(i, binning_scale)
+        noisy_image = apply_flat_field(noisy_image)  # apply circular aperture shadow
         
         # put sources in the image
         for j in range(N_sources):
             
             if j == variable_source:
-                filter_image = add_two_dimensional_gaussian_to_image(filter_image, *source_positions[j], peak_fluxes[j] + variable_function(i), 1, 1, 0)
+                noisy_image = add_two_dimensional_gaussian_to_image(noisy_image, *source_positions[j], peak_fluxes[j] + variable_function(i), 1, 1, 0)
             else:
-                filter_image = add_two_dimensional_gaussian_to_image(filter_image, *source_positions[j], peak_fluxes[j], 1, 1, 0)
+                noisy_image = add_two_dimensional_gaussian_to_image(noisy_image, *source_positions[j], peak_fluxes[j], 1, 1, 0)
         
         # create fits file
-        hdu = fits.PrimaryHDU(filter_image)
+        hdu = fits.PrimaryHDU(noisy_image)
         hdu.header["FILTER"] = fltr
         hdu.header["BINNING"] = f'{binning_scale}x{binning_scale}'
         hdu.header["GAIN"] = 1.
@@ -71,6 +136,15 @@ def create_image(filters, N_sources, variable_source, source_positions, peak_flu
 
 
 def main(overwrite: bool = False):
+    """
+    Create test data.
+    
+    Parameters
+    ----------
+    overwrite : bool, optional
+        Whether to overwrite test data if they currently exist, by default False.
+    """
+    
     # create directory if it does not exist
     if not os.path.isdir('Data/'):
         os.mkdir('Data/')
@@ -89,7 +163,7 @@ def main(overwrite: bool = False):
     print('Creating test data...')
     
     for i in tqdm(range(N_images)):
-        create_image(filters, N_sources, variable_source, source_positions, peak_fluxes, i, binning_scale, overwrite)
+        create_images(filters, N_sources, variable_source, source_positions, peak_fluxes, i, binning_scale, overwrite)
 
 if __name__ == "__main__":
     main()
