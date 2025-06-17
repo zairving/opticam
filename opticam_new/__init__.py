c61782518e7e2c9b28a8e87866e8fae0a0e5cf69
zairving <zackery.irving@icloud.com>
2025-06-17 11:57:51 +0100

    misc: helpers.py now imported in __init__.py

--- opticam_new/__init__.py
+++ opticam_new/__init__.py
@@ -5,7 +5,7 @@ from opticam_new.reducer import Reducer
 from opticam_new.photometer import Photometer
 from opticam_new.local_background import LocalBackground, CircularLocalBackground, EllipticalLocalBackground
 from opticam_new.correctors import FlatFieldCorrector
-
+from opticam_new import helpers
 
 
 
