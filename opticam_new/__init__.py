93a4df61ae3f043f75427847f0b325638ed8d03e
zairving <zackery.irving@icloud.com>
2025-07-11 17:29:43 +0100

    misc: updated to use new module names

--- opticam_new/__init__.py
+++ opticam_new/__init__.py
@@ -1,12 +1,11 @@
 
-from opticam_new.background import Background
-from opticam_new.finder import CrowdedFinder, Finder
-from opticam_new.catalog import Catalog
-from opticam_new.photometry import ForcedPhotometer, Photometer
+from opticam_new.background import DefaultBackground
+from opticam_new.finder import DefaultFinder
+from opticam_new.catalogue import Catalogue
 from opticam_new.differential_photometer import DifferentialPhotometer
-from opticam_new.local_background import LocalBackground, CircularLocalBackground, EllipticalLocalBackground
-from opticam_new.correctors import FlatFieldCorrector
 from opticam_new import helpers
+from opticam_new import photometers
+from opticam_new import correctors
 
 
 
