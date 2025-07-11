e4e5cf949fbb4ef6db2878eb7cc0af97d794ea6b
zairving <zackery.irving@icloud.com>
2025-07-11 17:22:53 +0100

    misc: renamed Finder to DefaultFinder and moved default npixels and border_width calculations to Catalogue

--- opticam_new/finder.py
+++ opticam_new/finder.py
@@ -2,79 +2,29 @@ from photutils.segmentation import SourceFinder, SegmentationImage
 from numpy.typing import NDArray
 
 
-class Finder:
+class DefaultFinder:
     """
-    Default source finder.
+    Default source finder. Combines image segmentation with source deblending.
     """
     
-    def __init__(self, npixels: int = None, border_width: int = None):
+    def __init__(self, npixels: int, border_width: int = 0):
         """
-        Default source finder.
+        Default source finder. Combines image segmentation with source deblending.
         
         Parameters
         ----------
-        npixels : int, optional
-            The minimum number of connected source pixels, by default 128 / W**2, where W is the image width.
+        npixels : int
+            The minimum number of connected source pixels.
         border_width : int, optional
-            The minimum distance from the image border for sources to be indentified, by default 1/16th of the image
-            width.
+            Sources within this many pixels of the border will be ignored, by default 0 (no sources are ignored).
         """
+        
+        assert type(npixels) is int and npixels > 0, '[OPTICAM] npixels must be a positive integer.'
         
         self.border_width = border_width
-        
-        if npixels is not None:
-            self.finder = SourceFinder(npixels, deblend=False, progress_bar=False)
-        else:
-            self.finder = None
+        self.finder = SourceFinder(npixels=npixels, deblend=True, progress_bar=False)
     
     def __call__(self, data: NDArray, threshold: float) -> SegmentationImage:
-        
-        if self.finder is None:
-            self.finder = SourceFinder(int(128 / (2048 / data.shape[0])**2), deblend=False, progress_bar=False)
-        
-        if self.border_width is None:
-            self.border_width = data.shape[0] // 16
-        
-        segment_map = self.finder(data, threshold)
-        
-        if self.border_width > 0:
-            segment_map.remove_border_labels(border_width=self.border_width, relabel=True)
-        
-        return segment_map
-
-
-class CrowdedFinder:
-    """
-    Crowded source finder. Similar to `Finder`, but with source deblending.
-    """
-    
-    def __init__(self, npixels: int = None, border_width: int = None):
-        """
-        Crowded source finder. Similar to `Finder`, but with source deblending.
-        
-        Parameters
-        ----------
-        npixels : int, optional
-            The minimum number of connected source pixels, by default 128 / W**2, where W is the image width.
-        border_width : int, optional
-            The minimum distance from the image border for sources to be indentified, by default 1/16th of the image
-            width.
-        """
-        
-        self.border_width = border_width
-        
-        if npixels is not None:
-            self.finder = SourceFinder(npixels=npixels, deblend=True, progress_bar=False)
-        else:
-            self.finder = None
-    
-    def __call__(self, data: NDArray, threshold: float) -> SegmentationImage:
-        
-        if self.finder is None:
-            self.finder = SourceFinder(int(128 / (2048 / data.shape[0])**2), deblend=True, progress_bar=False)
-        
-        if self.border_width is None:
-            self.border_width = data.shape[0] // 16
         
         segment_map = self.finder(data, threshold)
         
