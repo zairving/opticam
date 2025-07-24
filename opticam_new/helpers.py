e06712a15bf0d4ade7ccf63a6a68f97eaee95be7
zairving <zackery.irving@icloud.com>
2025-07-24 17:17:26 +0100

    feat: plot colours are now saved as a dict in helpers.py and added support for legacy filters (e.g., u' which is equivalent to u)

--- opticam_new/helpers.py
+++ opticam_new/helpers.py
@@ -20,10 +20,31 @@ bar_format= '{l_bar}{bar}|[{elapsed}<{remaining}]'
 
 # camera pixel scales
 pixel_scales = {
-    'u-band': 0.1397, 'g-band': 0.1397,
-    'r-band': 0.1406,
-    'i-band': 0.1661, 'z-band': 0.1661,
+    'u-band': 0.1397,  # camera 1
+    "u'-band": 0.1397,  # camera 1
+    'g-band': 0.1397,  # camera 1
+    "g'-band": 0.1397,  # camera 1
+    'r-band': 0.1406,  # camera 2
+    "r'-band": 0.1406,  # camera 2
+    'i-band': 0.1661,  # camera 3
+    "i'-band": 0.1661,  # camera 3
+    'z-band': 0.1661,  # camera 3
+    "z'-band": 0.1661,  # camera 3
     }
+
+# plotting colours for each filter
+colours = {
+    'u-band': 'tab:purple',  # camera 1
+    "u'-band": 'tab:purple',  # camera 1
+    'g-band': 'tab:green',  # camera 1
+    "g'-band": 'tab:green',  # camera 1
+    'r-band': 'tab:orange',  # camera 2
+    "r'-band": 'tab:orange',  # camera 2
+    'i-band': 'tab:olive',  # camera 3
+    "i'-band": 'tab:olive',  # camera 3
+    'z-band': 'tab:brown',  # camera 3
+    "z'-band": 'tab:brown',  # camera 3
+}
 
 # stdev -> FWHM scale factor
 fwhm_scale = 2 * np.sqrt(2 * np.log(2))
