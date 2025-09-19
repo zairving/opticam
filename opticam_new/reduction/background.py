435d6cfddaa9b7440b7f0a8af3c4f7677321e721
zairving <zackery.irving@icloud.com>
2025-09-19 10:15:15 +0100

    misc: improved docstrings and added validation to DefaultBackground

--- opticam_new/reduction/background.py
+++ opticam_new/reduction/background.py
@@ -1,5 +1,5 @@
 from photutils.background import Background2D
-from typing import Union, Tuple
+from typing import Tuple
 from numpy.typing import NDArray
 
 
@@ -16,9 +16,12 @@ class DefaultBackground:
         ----------
         box_size : int | Tuple[int, int]
             Size of the background mesh "pixels". If an integer is provided, the mesh pixels are squares of size
-            `box_size` x `box_size`. If a tuple is provided, the mesh pixels are rectangles of size
-            `box_size[0]` x `box_size[1]`.
+            `box_size` x `box_size`. If a tuple is provided, the mesh pixels are rectangles of width `box_size[1]` and
+            height `box_size[0]`.
         """
+        
+        if not isinstance(box_size, int):
+            assert len(box_size) == 2, "[OPTICAM] Incompatible box_size parameter passed to DefaultBackground. box_size must be either an integer or an interable of dimensions (e.g., [height, width])."
         
         self.box_size = box_size
     
