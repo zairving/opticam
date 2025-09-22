bfd8040a2638e70ffb3a37ff0ec14b7828d5563a
zairving <zackery.irving@icloud.com>
2025-09-22 15:40:07 +0100

    docs: improved docstrings in transforms.py

--- opticam_new/reduction/transforms.py
+++ opticam_new/reduction/transforms.py
@@ -8,9 +8,25 @@ def find_translation(
     coords: NDArray,
     reference_coords: NDArray
     ) -> SimilarityTransform:
+    """
+    Find the translation that maps `coords` onto `reference_coords`.
+    
+    Parameters
+    ----------
+    coords : NDArray
+        The source coordinates.
+    reference_coords : NDArray
+        The reference source coordinates.
+    
+    Returns
+    -------
+    SimilarityTransform
+        The transformation matrix that maps `coords` onto `reference_coords`.
+    """
     
     distance_matrix = cdist(reference_coords, coords)
-    reference_indices, indices = linear_sum_assignment(distance_matrix)  # match sources across images
+    reference_indices, indices = linear_sum_assignment(distance_matrix)
+    
     dx = np.mean(reference_coords[reference_indices, 0] - coords[indices, 0])
     dy = np.mean(reference_coords[reference_indices, 1] - coords[indices, 1])
     
