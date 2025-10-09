d3f9e11b1aaec305aaf90cb28eeba6bcd3cc0a95
Zac Irving <zackery.irving@icloud.com>
2025-10-09 13:49:03 +0100

    misc: changed SimplePhotometer to the more apt AperturePhotometer

--- opticam/__init__.py
+++ opticam/__init__.py
@@ -4,7 +4,7 @@ from opticam.background.local_background import BaseLocalBackground, DefaultLoca
 from opticam.finders import DefaultFinder
 from opticam.reducer import Reducer
 from opticam.analysis.differential_photometer import DifferentialPhotometer
-from opticam.photometers import SimplePhotometer, OptimalPhotometer
+from opticam.photometers import AperturePhotometer, OptimalPhotometer
 from opticam.correctors.flat_field_corrector import FlatFieldCorrector
 from opticam.analysis.analyzer import Analyzer
 from opticam.utils.generate import generate_flats, generate_observations, generate_gappy_observations
